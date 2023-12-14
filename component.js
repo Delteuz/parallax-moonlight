7673950336f5710b166c8ccbb60c7fffbcb75ad7
= <=>
2023-12-15 00:31:31 +0800

    music added

---
+++ component.js
@@ -0,0 +1,48 @@
+function goBack1() {
+  container1.classList.remove("right-panel-active");
+}
+function goToHint1() {
+  container1.classList.add("right-panel-active");
+}
+
+function goBack2() {
+  container2.classList.remove("right-panel-active");
+}
+function goToHint2() {
+  container2.classList.add("right-panel-active");
+}
+
+function goBack3() {
+  container3.classList.remove("right-panel-active");
+}
+function goToHint3() {
+  container3.classList.add("right-panel-active");
+}
+
+function goBack4() {
+  container4.classList.remove("right-panel-active");
+}
+function goToHint4() {
+  container4.classList.add("right-panel-active");
+}
+
+function goBack5() {
+  container5.classList.remove("right-panel-active");
+}
+function goToHint5() {
+  container5.classList.add("right-panel-active");
+}
+
+// // Create a new Audio object
+// var audio = new Audio();
+
+// // Set the attributes for the audio object
+// audio.autoplay = true;
+// audio.loop = true;
+// audio.volume = 100;
+
+// // Set the audio source
+// audio.src = "magic-in-the-air.mp3"; // Replace with your audio file path
+
+// // Append the audio element to the webpage
+// document.getElementById("audio-container").appendChild(audio);
